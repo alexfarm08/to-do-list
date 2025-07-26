4a1d74763d95fca97f5734edacc52be928057c59
Alex <alex@Alexs-MacBook-Pro.local>
2025-07-25 18:52:45 -0700

    took out debugging console.logs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import {addDate} from "./dates.js";
 import { addToDo, arrToDo } from "./toDoLogic.js";
 
-console.log(addDate(2024, 8, 22));
-
-addToDo("go to bar", "going to bar with eric", 2024, 8, 22, "medium");
-
-console.log(arrToDo);
